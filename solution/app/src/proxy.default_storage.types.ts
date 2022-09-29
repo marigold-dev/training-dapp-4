9e824aac47f5a6f847c91d0af137308bbd8f9967
Benjamin Fuentes <benjamin.fuentes@marigold.dev>
2022-09-29 17:30:28 +0200

    moving to taqueria

---
+++ solution/app/src/proxy.default_storage.types.ts
@@ -0,0 +1,19 @@
+
+import { ContractAbstractionFromContractType, WalletContractAbstractionFromContractType } from './type-utils';
+import {  } from './type-aliases';
+
+type Storage = {
+    
+};
+
+type Methods = {
+    
+};
+
+type MethodsObject = {
+    
+};
+
+type contractTypes = { methods: Methods, methodsObject: MethodsObject, storage: Storage, code: { __type: 'ProxyDefaultStorageCode', protocol: string, code: object[] } };
+export type ProxyDefaultStorageContractType = ContractAbstractionFromContractType<contractTypes>;
+export type ProxyDefaultStorageWalletType = WalletContractAbstractionFromContractType<contractTypes>;
